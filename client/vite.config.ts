fe13e24baa9d5de71201d71fe80137cc4ff3085d
Alexandra Pastor <pastoralex@free.fr>
2025-07-24 22:12:17 +0200

    fix vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./",
   server: {
     port: 3000,
   },
