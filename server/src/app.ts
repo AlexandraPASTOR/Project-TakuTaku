ebf714fdba9a432c5147178f675134f8e9a24b52
Alexandra Pastor <pastoralex@free.fr>
2025-10-27 10:46:45 +0100

    add methods : options

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ const app = express();
   app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
