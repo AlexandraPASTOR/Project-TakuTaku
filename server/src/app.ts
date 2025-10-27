3c587a95a23f2b49f18119305d5b03870cca92a3
Alexandra Pastor <pastoralex@free.fr>
2025-10-27 13:31:03 +0100

    update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,10 @@ app.use(((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+res.setHeader(
+  "Access-Control-Allow-Headers",
+  "Content-Type, Authorization, Origin"
+);
   
   // ✅ Fix pour Cloudflare/Render
   res.setHeader("Vary", "Origin");
