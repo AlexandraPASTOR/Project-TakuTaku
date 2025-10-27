ef592fcc336d9a503a575799f78053f81da68a91
Alexandra Pastor <pastoralex@free.fr>
2025-10-27 13:21:29 +0100

    update userContext

--- client/context/UserContext.tsx
+++ client/context/UserContext.tsx
@@ -62,7 +62,6 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
           headers: {
             "Content-Type": "application/json",
           },
-          credentials: "include",
           body: JSON.stringify(newUser),
         },
       );
